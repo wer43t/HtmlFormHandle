4bd65cbd13d8dac30ce020ccfa467cd38ce8e135
Злой Чай, который Салават <abrarov03@gmail.com>
2021-10-13 07:40:18 +0300

    Добавьте файлы проекта.

---
+++ HtmlFormHandle/Core.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class FishType
+    {
+        public string typeName { get; set; }
+        public int minTemp { get; set; }
+        public int maxTemp { get; set; }
+        public int minTime { get; set; }
+        public int maxTime { get; set; }
+
+        public FishType(string newTypeName, int newMinTemp, int newMaxTemp, int newMinTime, int newMaxTime)
+        {
+            typeName = newTypeName;
+            minTemp = newMinTemp;
+            maxTemp = newMaxTemp;
+            minTime = newMinTime;
+            maxTime = newMaxTime;
+        }
+        public FishType(string newTypeName) : this(newTypeName, 0, 0, 0, 0)
+        { }
+
+    }
+    public class Fish
+    {
+        public FishType fishType { get; set; }
+
+        public Fish(FishType newFishType, List<DateTime> newTime, List<int> newTemp)
+        {
+            fishType = newFishType;
+        }
+        public Fish(string newFishType)
+        {
+            fishType = new FishType(newFishType);
+        }
+
+    }
+    public class Car
+    {
+        public string carName { get; set; }
+        public List<Fish> fishes { get; set; }
+        public DateTime date { get; set; }
+        public List<DateTime> time { get; set; }
+        public List<int> temp { get; set; }
+
+        public Car()
+        {
+            carName = "Car";
+            fishes = new List<Fish>();
+            date = new DateTime();
+        }
+        public string Delivery(string startTime, string deliveryTemp)
+        {
+            string result = "<table border=1>";
+            foreach (Fish f in fishes)
+            {
+                int minMistake = 0;
+                int maxMistake = 0;
+                string mistakes = "";
+                string status = "";
+                FishType fish = f.fishType;
+
+                DateTime start = DateTime.Parse(startTime);
+                foreach (string t in deliveryTemp.Split(' '))
+                {
+                    if (int.Parse(t) > fish.maxTemp)
+                    {
+                        if (mistakes == "")
+                        {
+                            mistakes += "<tr><th>Время</th><th>Факт</th><th>Норма</th><th>Отклонение</th></tr>";
+                        }
+                        maxMistake += 10;
+                        mistakes += $"<tr><th>{start}</th> <th>{t}</th> <th>{fish.maxTemp}</th> <th>{fish.maxTemp - int.Parse(t)}</th></tr>";
+                    }
+                    else if (int.Parse(t) < fish.minTemp)
+                    {
+                        if (mistakes == "")
+                        {
+                            mistakes += "<tr><th>Время</th><th>Факт</th><th>Норма</th><th>Отклонение</th></tr>";
+                        }
+                        minMistake += 10;
+                        mistakes += $"<tr><th>{start}</th> <th>{t}</th> <th>{fish.minTemp}</th> <th>{fish.minTemp - int.Parse(t)}</th></tr>";
+                    }
+                    start = start.AddMinutes(10);
+                }
+
+                if (minMistake > fish.minTime )
+                {
+                    status = $"<tr><td>Вид рыб</td><td>{fish.typeName}</td><td></td><td></td></tr><tr><td></td><td>Температура</td><td>Время</td><td></td></tr><tr><td>Max</td><td>{fish.maxTemp}</td><td>{fish.maxTime}</td><td></td></tr><tr><td>Min</td><td>{fish.minTemp}</td><td>{ fish.minTime}</td><td></td></tr><tr><td>Дата</td><td>{startTime}</td><td></td><td></td></tr><tr><td>Температура</td><td colspan='3'>{deliveryTemp}</td></tr><tr><td>Отчет</td><td></td><td></td><td></td></tr><tr><th colspan='4'>Порог минимально допустимой температуры превышен на { minMistake}минут.</th></tr>";
+                }
+                else if (maxMistake > fish.maxTime)
+                {
+                    status = $"<tr><td>Вид рыб</td><td>{fish.typeName}</td><td></td><td></td></tr><tr><td></td><td>Температура</td><td>Время</td><td></td></tr><tr><td>Max</td><td>{fish.maxTemp}</td><td>{fish.maxTime}</td><td></td></tr><tr><td>Min</td><td>{fish.minTemp}</td><td>{ fish.minTime}</td><td></td></tr><tr><td>Дата</td><td>{startTime}</td><td></td><td></td></tr><tr><td>Температура</td><td colspan='3'>{deliveryTemp}</td></tr><tr><td>Отчет</td><td></td><td></td><td></td></tr><tr><th colspan='4'>Порог минимально допустимой температуры превышен на { maxMistake}минут.</th></tr>";
+                }
+                else
+                {
+                    status = $"<tr><td>Вид рыб</td><td>{fish.typeName}</td><td></td><td></td></tr><tr><td></td><td>Температура</td><td>Время</td><td></td></tr><tr><td>Max</td><td>{fish.maxTemp}</td><td>{fish.maxTime}</td><td></td></tr><tr><td>Min</td><td>{fish.minTemp}</td><td>{ fish.minTime}</td><td></td></tr><tr><td>Дата</td><td>{startTime}</td><td></td><td></td></tr><tr><td>Температура</td><td colspan='3'>{deliveryTemp}</td></tr><tr><td>Отчет</td><td></td><td></td><td></td></tr><tr><th colspan='4'>Доставка выполнена успешно</th></tr>";
+                }
+
+                result += status + mistakes + "</table>";
+            }
+            return result;
+        }
+    }
+}
---
+++ HtmlFormHandle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Core;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            Console.WriteLine("Content-Type: text/html \n\n");
+            var queryStr = Environment.GetEnvironmentVariable("QUERY_STRING");
+
+            //var queryStr = "FishName=Semga&MinTemp=-3&MaxTemp=5&MinTime=20&MaxTime=60&TimeStart=12.06.2021+12%3A23&Temps=1+2+3+3+4+3+5+4+2+1+1+1+1+0+-1+-2+-2+-3+-4+-4+-5+-5+-4+-4+-4+-3";
+            queryStr = WebUtility.UrlDecode(queryStr);
+            string[] queryArr = queryStr.Split('&');
+
+            //foreach (string q in queryArr)
+            //{
+            //    Console.WriteLine(q);
+            //}
+
+
+            //int fishCount = int.Parse(Console.ReadLine());                      //  Количество рыб
+
+            //for (int i = 0; i < fishCount; i++)
+            //{
+            car.fishes.Add(new Fish(queryArr[0].Split('=')[1]));                   //  Название рыбы
+            car.fishes[0].fishType.minTemp = int.Parse(queryArr[1].Split('=')[1]); //  Минимальная температура      
+            car.fishes[0].fishType.maxTemp = int.Parse(queryArr[2].Split('=')[1]); //  Максимальная температура
+            car.fishes[0].fishType.minTime = int.Parse(queryArr[3].Split('=')[1]); //  Минимальное время
+            car.fishes[0].fishType.maxTime = int.Parse(queryArr[4].Split('=')[1]); //  Максимальное время
+                                                                                   //}
+
+            Console.WriteLine($"<pre>{car.Delivery(queryArr[5].Split('=')[1], queryArr[6].Split('=')[1])}</pre>"); // Время старта, Температура
+        }
+    }
+}
